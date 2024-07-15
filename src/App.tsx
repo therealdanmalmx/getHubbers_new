e212891af9701889011b777fdb77897bc9a87b22
therealdanmalmx <danmalmx@gmail.com>
2024-07-15 17:56:42 +0100

    Scaffold react. Add tailwind. Add iconsData

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return <div className='text-7xl text-center'>GetHubbers</div>;
+}
+
+export default App;
---
+++ src/data/iconsData.tsx
@@ -0,0 +1,173 @@
+import angular from "../../public/icons/angular.svg";
+import c from "../../public/icons/c.svg";
+import csharp from "../../public/icons/c#.svg";
+import cplusplus from "../../public/icons/c++.svg";
+import django from "../../public/icons/django.svg";
+import go from "../../public/icons/go.svg";
+import java from "../../public/icons/java.svg";
+import javascript from "../../public/icons/javascript.svg";
+import kotlin from "../../public/icons/kotlin.svg";
+import laravel from "../../public/icons/laravel.svg";
+import dotnet from "../../public/icons/.NET.svg";
+import dotnetcore from "../../public/icons/.NETcore.svg";
+import node from "../../public/icons/.node.svg";
+import php from "../../public/icons/.php.svg";
+import python from "../../public/icons/.python.svg";
+import rails from "../../public/icons/.rails.svg";
+import react from "../../public/icons/.react.svg";
+import ruby from "../../public/icons/.ruby.svg";
+import rust from "../../public/icons/.rust.svg";
+import spring from "../../public/icons/.spring.svg";
+import svelte from "../../public/icons/.svelte.svg";
+import swift from "../../public/icons/.swift.svg";
+import typescript from "../../public/icons/.typescript.svg";
+import vue from "../../public/icons/.vue.svg";
+
+const iconsData = [
+  {
+    id: 1,
+    name: "angular",
+    icon: angular,
+    type: "framework",
+  },
+  {
+    id: 2,
+    name: "c",
+    icon: c,
+    type: "language",
+  },
+  {
+    id: 3,
+    name: "c#",
+    icon: csharp,
+    type: "language",
+  },
+  {
+    id: 4,
+    name: "c++",
+    icon: cplusplus,
+    type: "language",
+  },
+  {
+    id: 5,
+    name: "django",
+    icon: django,
+    type: "framework",
+  },
+  {
+    id: 6,
+    name: "go",
+    icon: go,
+    type: "language",
+  },
+  {
+    id: 7,
+    name: "java",
+    icon: java,
+    type: "language",
+  },
+  {
+    id: 8,
+    name: "javascript",
+    icon: javascript,
+    type: "language",
+  },
+  {
+    id: 9,
+    name: "kotlin",
+    icon: kotlin,
+    type: "language",
+  },
+  {
+    id: 10,
+    name: "laravel",
+    icon: laravel,
+    type: "framework",
+  },
+  {
+    id: 11,
+    name: "dotnet",
+    icon: dotnet,
+    type: "framework",
+  },
+  {
+    id: 12,
+    name: "dotnetcore",
+    icon: dotnetcore,
+    type: "framework",
+  },
+  {
+    id: 13,
+    name: "node",
+    icon: node,
+    type: "framework",
+  },
+  {
+    id: 14,
+    name: "php",
+    icon: php,
+    type: "language",
+  },
+  {
+    id: 15,
+    name: "python",
+    icon: python,
+    type: "language",
+  },
+  {
+    id: 16,
+    name: "rails",
+    icon: rails,
+    type: "framework",
+  },
+  {
+    id: 17,
+    name: "react",
+    icon: react,
+    type: "framework",
+  },
+  {
+    id: 18,
+    name: "ruby",
+    icon: ruby,
+    type: "language",
+  },
+  {
+    id: 19,
+    name: "rust",
+    icon: rust,
+    type: "language",
+  },
+  {
+    id: 20,
+    name: "spring",
+    icon: spring,
+    type: "framework",
+  },
+  {
+    id: 21,
+    name: "svelte",
+    icon: svelte,
+    type: "framework",
+  },
+  {
+    id: 22,
+    name: "swift",
+    icon: swift,
+    type: "language",
+  },
+  {
+    id: 23,
+    name: "typescript",
+    icon: typescript,
+    type: "language",
+  },
+  {
+    id: 24,
+    name: "vue",
+    icon: vue,
+    type: "framework",
+  },
+];
+
+export default iconsData;
