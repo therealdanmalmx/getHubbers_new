38c3924278f50793b59288a24febc66d37803ead
therealdanmalmx <danmalmx@gmail.com>
2024-07-15 19:13:16 +0100

    Add navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 // import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
-import gethubberslogo from "/logos/gethubberslogo_s.svg";
+import gethubberslogo from "../../public/logos/gethubberslogo_s.png";
 
 const NavBar = () => {
   //   const { t } = useTranslation();
