3d76f14e37a20ce14f60c3f5ca3326080707b591
therealdanmalmx <danmalmx@gmail.com>
2024-08-09 22:13:22 +0200

    Clear timeout upn onClick

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,21 +1,39 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AlertContext } from "../Context/AlertContext";
 
 const Alert = () => {
   const { alertText, setShowAlert, showAlert } = useContext(AlertContext);
+  let timeoutId: NodeJS.Timeout | null = null;
+
+  useEffect(() => {
+    if (showAlert) {
+      timeoutId = setTimeout(() => {
+        setShowAlert(false);
+      }, 5000);
+    }
+
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [showAlert, setShowAlert]);
+
+  const closeTheThing = () => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    setShowAlert(false);
+  };
 
   if (!showAlert) {
     return null;
   }
 
-  setTimeout(() => {
-    setShowAlert(false);
-  }, 5000);
-
   return (
     <div className="flex h-14 w-full items-center justify-between bg-red-600 px-8 text-white">
       <div>{alertText}</div>
-      <div className="cursor-pointer" onClick={() => setShowAlert(false)}>
+      <div className="cursor-pointer" onClick={closeTheThing}>
         X
       </div>
     </div>
