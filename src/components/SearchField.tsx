3b7d1d3dbd136e68827fbbd8c5a08762ba211c25
therealdanmalmx <danmalmx@gmail.com>
2026-01-16 16:24:56 +0100

    Set formated country to translation fileld

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -9,6 +9,7 @@ const SearchField = () => {
 
   const { t } = useTranslation();
 
+  console.log({formattedCountry})
   return (
     <div>
       <h1 className="m-4 mx-2 w-full text-balance text-center text-2xl xl:w-[500px]">
@@ -19,14 +20,18 @@ const SearchField = () => {
       <div className="mx-4 flex items-center justify-center md:mx-0">
         <input
           type="text"
-          placeholder={t("searchFieldPlaceholder")}
+          placeholder={t("searchFieldPlaceholder", {
+          formattedCountry,
+        })}
           className="w-5/6 border-2 border-r-0 border-gray-500 p-4 outline-none md:w-96"
         />
         <button
           onClick={getSearchCity}
           className="border-2 border-l-0 border-black bg-black p-4 text-white"
         >
-          {t("searchButton")}
+          {t("searchButton", {
+          formattedCountry,
+        })}
         </button>
       </div>
     </div>
