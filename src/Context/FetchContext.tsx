22d537042eafed48574a8144752baf235d100553
therealdanmalmx <danmalmx@gmail.com>
2026-01-17 19:45:27 +0100

    Add github profile type. Remoe callback

--- src/Context/FetchContext.tsx
+++ src/Context/FetchContext.tsx
@@ -3,15 +3,42 @@ import { FC, ReactNode, createContext, useCallback, useContext, useState } from 
 import { useTranslation } from "react-i18next";
 import { AlertContext } from "./AlertContext";
 
+type GithubProfilesResponse = {
+  incomplete_results: boolean;
+  items: Array<{
+    id: number;
+    avatar_url: string;
+    events_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    gravatar_id: string;
+    html_url: string;
+    login: string;
+    node_id: string;
+    organizations_url: string;
+    received_events_url: string;
+    repos_url: string;
+    score: number;
+    site_admin: boolean;
+    starred_url: string;
+    subscriptions_url: string;
+    type: GithubProfilesResponse;
+    url: string;
+    user_view_type: string;
+  }>;
+  total_count: number;
+};
+
 type FetchContextType = {
-  profiles: string[];
+  profiles: GithubProfilesResponse;
   profile: Record<string, any>;
   getHubberProfiles: (selectedIcons: string[], city: string) => Promise<void>;
   getIndividualProfile: (login: string) => Promise<void>;
 };
 
 export const FetchContext = createContext<FetchContextType>({
-  profiles: [],
+  profiles: { items: [], total_count: 0, incomplete_results: false },
   profile: {},
   getHubberProfiles: async () => {},
   getIndividualProfile: async () => {},
@@ -19,15 +46,14 @@ export const FetchContext = createContext<FetchContextType>({
 
 export const FetchProvider: FC<{ children: ReactNode }> = ({ children }) => {
 
-  const [profiles, setProfiles] = useState<string[]>([]);
-  const [profile, setProfile] = useState<Record<string, any>>({});
+const [profiles, setProfiles] = useState<GithubProfilesResponse>({ items: [], total_count: 0, incomplete_results: false });  const [profile, setProfile] = useState<Record<string, any>>({});
   const { setAlertText } = useContext(AlertContext);
   const { t } = useTranslation();
 
-const getHubberProfiles = useCallback(async (selectedIcons: string[], city: string) => {
+const getHubberProfiles = async (selectedIcons: string[], city: string) => {
   if (city !== undefined) {
     try {
-      const query = `language:${selectedIcons.join("+")}+location:${city || "Sweden"}`
+      const query = `language:${selectedIcons.join("+")}+location:${city ? city : "Sweden" }`
       const res = await axios.get(
         `https://api.github.com/search/users?q=${query}&client_id=${import.meta.env.VITE_GH_CID}&client_secret=${import.meta.env.VITE_GH_CSC}`,
       );
@@ -43,7 +69,7 @@ const getHubberProfiles = useCallback(async (selectedIcons: string[], city: stri
         setAlertText(t("fetcherror"));
     }
   }
-}, [setAlertText, t]);
+};
 
 const getIndividualProfile = useCallback(async (login: string) => {
   try {
