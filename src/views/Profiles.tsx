fb6a157108ee78dac14916b82134b0256522d648
therealdanmalmx <danmalmx@gmail.com>
2026-01-18 20:05:25 +0100

    [WIP] handle indvidual profile

--- src/views/Profiles.tsx
+++ src/views/Profiles.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { FetchContext } from "../Context/FetchContext";
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Profiles = () => {
   const { profiles, getIndividualProfile } = useContext(FetchContext)
@@ -8,9 +8,9 @@ const Profiles = () => {
 
   const navigate = useNavigate();
 
-  const handleProfileClick = async (login: string, e: React.MouseEvent) => {
+  const handleProfileClick = (login: string, e: React.MouseEvent) => {
     e.preventDefault();
-    await getIndividualProfile(login);
+    getIndividualProfile(login);
     navigate(`/profile/${login}`);
   };
 
@@ -19,14 +19,15 @@ const Profiles = () => {
       <div>Profile</div>
       <div className="flex mx-auto flex-wrap gap-8 p-8">
          {profiles?.items?.map((profile) => (
-          <div
-            key={profile.id}
-            className="text-center cursor-pointer"
-            onClick={(e) => handleProfileClick(profile.login, e)}
-          >
-            <img src={profile.avatar_url} alt={profile.login} className="size-48 rounded-full mx-auto"/>
-            <span>{profile.login}</span>
-          </div>
+          <Link to={`/profile/${profile.login}`}>
+            <div
+              key={profile.id}
+              className="text-center cursor-pointer"
+            >
+              <img src={profile.avatar_url} alt={profile.login} className="size-48 rounded-full mx-auto"/>
+              <span>{profile.login}</span>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
