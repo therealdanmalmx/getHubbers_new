8cddec8b0aab4558eb51c50d95dc7683775b3c8c
therealdanmalmx <danmalmx@gmail.com>
2026-01-20 09:02:51 +0100

    Add card styling. Remove unised code.

--- src/views/Profiles.tsx
+++ src/views/Profiles.tsx
@@ -1,33 +1,26 @@
 import { useContext } from "react";
+import { Link } from 'react-router-dom';
 import { FetchContext } from "../Context/FetchContext";
-import { Link, useNavigate } from 'react-router-dom';
 
 const Profiles = () => {
-  const { profiles, getIndividualProfile } = useContext(FetchContext)
+  const { profiles } = useContext(FetchContext)
   console.log({profiles});
-
-  const navigate = useNavigate();
-
-  const handleProfileClick = (login: string, e: React.MouseEvent) => {
-    e.preventDefault();
-    getIndividualProfile(login);
-    navigate(`/profile/${login}`);
-  };
 
   return (
     <div>
-      <div>Profile</div>
       <div className="flex mx-auto flex-wrap gap-8 p-8">
-         {profiles?.items?.map((profile) => (
-          <Link to={`/profile/${profile.login}`}>
-            <div
-              key={profile.id}
-              className="text-center cursor-pointer"
-            >
-              <img src={profile.avatar_url} alt={profile.login} className="size-48 rounded-full mx-auto"/>
-              <span>{profile.login}</span>
-            </div>
-          </Link>
+        {profiles?.items?.map((profile) => (
+          <div
+            key={profile.id}
+            className="text-center cursor-pointer relative"
+          >
+            <img src={profile.avatar_url} alt={profile.login} className="h-96 w-64 b-white p-2 border-slate-400 border-2 rounded-xl object-cover mx-auto"/>
+            <Link to={`/profile/${profile.login}`}>
+              <div className="absolute inset-4 flex items-end justify-end">
+                <button className="px-4 py-2 bg-white hover:bg-opacity-50 transition-colors duration-300 ease-in-out rounded-full">{profile.login}</button>
+              </div>
+            </Link>
+          </div>
         ))}
       </div>
     </div>
