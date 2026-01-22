498a31542bf4d8ead45a1f4a2850c0bf85d95f72
therealdanmalmx <danmalmx@gmail.com>
2026-01-22 10:13:53 +0100

    Move not/hireable circle to end on mobile

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -32,9 +32,9 @@ const Profile = () => {
         <div className="flex lg:flex-row flex-col w-full justify-between">
           <div className="mx-4 flex flex-col items-start justify-between space-y-2">
             <div>
-              <div className="flex items-center mt-2">
+              <div className="flex items-center justify-between lg:justify-start mt-2">
                 <p className="text-2xl font-bold">{profile.name}</p>
-                {profile.hireable ? <FaCheckCircle className="size-8 text-green-700 mx-4" title="available for hire"/> : <GoXCircleFill className="size-8 text-red-700 mx-4" title="not available for hire" />}
+                {profile.hireable ? <FaCheckCircle className="size-8 text-green-700 lg:mx-6" title="available for hire"/> : <GoXCircleFill className="size-8 text-red-700 lg:mx-6" title="not available for hire" />}
               </div>
               <div className="space-y-4 lg:space-y-0">
                 {profile.company && <p className="text-xl"> {profile.company}</p>}
