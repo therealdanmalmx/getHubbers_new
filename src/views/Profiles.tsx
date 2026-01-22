af56cd8c9ccc3ec8b6ede44a1d4a3ba27d750b06
therealdanmalmx <danmalmx@gmail.com>
2026-01-22 10:05:41 +0100

    Center and make cards wider on mobile

--- src/views/Profiles.tsx
+++ src/views/Profiles.tsx
@@ -8,13 +8,13 @@ const Profiles = () => {
 
   return (
     <div>
-      <div className="flex mx-auto flex-wrap gap-8 p-8">
+      <div className="flex justify-center flex-wrap gap-8 p-8">
         {profiles?.items?.map((profile) => (
           <div
             key={profile.id}
             className="text-center cursor-pointer relative"
           >
-            <img src={profile.avatar_url} alt={profile.login} className="h-96 w-64 b-white p-2 border-slate-100 border-2 rounded-xl object-cover mx-auto"/>
+            <img src={profile.avatar_url} alt={profile.login} className="h-96 w-96 lg:w-64 b-white p-2 border-slate-100 border-2 rounded-xl object-cover mx-auto"/>
             <Link to={`/profile/${profile.login}`}>
               <div className="absolute inset-4 flex items-end justify-end">
                 <button className="px-4 py-2 bg-white hover:bg-opacity-50 transition-colors duration-300 ease-in-out rounded-full">{profile.login}</button>
