58903c6f74c6fcc60472e0014434d24f0903e1e4
therealdanmalmx <danmalmx@gmail.com>
2026-01-28 09:27:27 +0100

    Type C# in browser

--- src/views/Profiles.tsx
+++ src/views/Profiles.tsx
@@ -16,7 +16,7 @@ const Profiles = () => {
 
   return (
     <div>
-      {profiles.items.length > 0 && <div className="text-center text-2xl lg:text-5xl font-bold uppercase my-4">{selectedIcons.join(", ")} {t("developers")} | {searchText ? searchText : formattedCountry} </div>}
+      {profiles.items.length > 0 && <div className="text-center text-2xl lg:text-5xl font-bold uppercase my-4">{selectedIcons.map((icon => icon === "csharp" ? "C#" : icon)).join(", ")} {t("developers")} | {searchText ? searchText : formattedCountry} </div>}
       <div className="flex justify-center flex-wrap gap-8 p-8">
         {profiles?.items?.map((profile) => (
           <div
