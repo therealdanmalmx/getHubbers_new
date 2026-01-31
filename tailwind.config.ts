70064bb0d3be535eccec992a4d15a314755b60e1
therealdanmalmx <danmalmx@gmail.com>
2026-01-31 18:31:06 +0100

    Add small-screen definition

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -11,6 +13,10 @@ export default {
         "!text-green-500": "#10B981 !important",
         "!text-white": "#FFFFFF !important",
       },
+      screens: {
+        "small-screen": {"max": "376px"},
+        ...defaultTheme.screens,
+      }
     },
   },
   plugins: [],
