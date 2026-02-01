21bc783e9a7bdaa8a8fb96c7587e2421706f85da
therealdanmalmx <danmalmx@gmail.com>
2026-02-01 10:08:48 +0100

    Change styling

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -32,7 +32,7 @@ const Alert = () => {
   }
 
   return (
-    <div className="flex h-24 w-full items-center justify-between bg-red-600 px-6 text-white transition duration-1000 ease-in-out md:justify-center xl:h-14">
+    <div className="flex h-16 w-full items-center justify-between bg-red-600 px-6 text-white transition duration-1000 ease-in-out md:justify-center xl:h-14">
       <div className="md:flex-1 w-10/12 lg:w-full md:text-center">{alertText}</div>
       <div className="cursor-pointer md:mx-auto" onClick={closeAlert}>
         <CloseIcon className="size-6 transition duration-300 hover:size-5" />
