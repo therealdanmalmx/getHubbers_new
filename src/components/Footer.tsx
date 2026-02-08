3b2046892bd52f2d89549f44430467223dd5e2bd
therealdanmalmx <danmalmx@gmail.com>
2026-02-08 17:34:02 +0100

    Reduce footer height

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import DropdownCountries from './DropdownCountries'
 
 const Footer = () => {
   return (
-    <div className='h-48 lg:h-36 flex flex-col-reverse lg:flex-row lg:justify-between gap-8 lg:gap-0  px-12 bg-navbar absolute w-full bottom-0 items-center justify-center'>
+    <div className='h-40 lg:h-28 flex flex-col-reverse lg:flex-row lg:justify-between gap-8 lg:gap-0 px-12 bg-navbar fixed w-full bottom-0 items-center justify-center'>
       <div>
         <a href="https://www.buymeacoffee.com/danmalmx" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style={{height: "40px", width: "152px"}} /></a>
       </div>
