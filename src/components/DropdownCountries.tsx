1ca9b02ccc3ad7f501af29f1a9017adc45b60fdf
therealdanmalmx <danmalmx@gmail.com>
2026-09-14 22:13:59 +0200

    Change default dropdown

--- src/components/DropdownCountries.tsx
+++ src/components/DropdownCountries.tsx
@@ -26,7 +26,7 @@ const DropdownCountries = () => {
     const savedCode = localStorage.getItem("country_code");
     return (
       countryList.find((c) => c.code.toLowerCase() === savedCode) ||
-      countryList[13]
+      countryList[12]
     );
   };
 
