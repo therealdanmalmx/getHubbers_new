72d25e837c0a7e24e306a90c257f8688866859a8
therealdanmalmx <danmalmx@gmail.com>
2026-09-15 19:41:42 +0200

    Use localStorage utils

--- src/views/SavedList.tsx
+++ src/views/SavedList.tsx
@@ -2,55 +2,69 @@ import { useEffect, useState } from "react";
 import { FaChevronCircleLeft } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
 
-import { Link, useNavigate } from 'react-router-dom';
-import { profileList } from "../utils/ProfileList";
 import { useTranslation } from "react-i18next";
+import { Link, useNavigate } from "react-router-dom";
+import { profileList } from "../utils/ProfileList";
+import { getSavedProfiles } from "../utils/savedList";
 
 const SavedList = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { t } = useTranslation();
-  const [profiles, setProfiles] = useState(profileList);
+  const [savedList, setSavedList] = useState(getSavedProfiles);
 
   const removeProfile = (id: number) => {
-    const index = profileList.findIndex((p) => p.id === id);
+    const index = savedList.findIndex((p) => p.id === id);
     if (index > -1) {
       profileList.splice(index, 1);
-      setProfiles(profiles.filter((p) => p.id !== id));
-  }
-}
+      setSavedList(savedList.filter((p) => p.id !== id));
+    }
+  };
 
-useEffect(() => {
+  useEffect(() => {
+    if (!savedList.length) {
+      navigate("/profiles");
+    }
+  }, [savedList]);
 
-  if(!profiles.length)
-  {
-    navigate("/profiles")
-  }
-}, [profiles]);
-
-return (
-  <div>
-    <div className="flex flex-col lg:flex-row justify-center items-center my-4">
-      <div onClick={() => navigate(-1)}><FaChevronCircleLeft className="size-12 mx-auto my-2 lg:mx-16 cursor-pointer hover:bg-slate-500 hover:rounded-full"/></div>
-        {profileList.length > 0 && <div className="lg:flex-1 text-center text-2xl lg:text-5xl font-bold uppercase">{t("savedProfiles")}</div>}
-    </div>
-    <div className="flex justify-center flex-wrap gap-8">
-      {profiles?.map((profile) => (
-        <div
-          key={profile.id}
-          className="text-center cursor-pointer relative"
-        >
-          <img src={profile.avatar_url} alt={profile.login} className="h-96 w-96 lg:w-64 b-white p-2 border-slate-100 border-2 rounded-xl object-cover mx-auto"/>
+  return (
+    <div>
+      <div className="my-4 flex flex-col items-center justify-center lg:flex-row">
+        <div onClick={() => navigate(-1)}>
+          <FaChevronCircleLeft className="mx-auto my-2 size-12 cursor-pointer hover:rounded-full hover:bg-slate-500 lg:mx-16" />
+        </div>
+        {savedList.length > 0 && (
+          <div className="text-center text-2xl font-bold uppercase lg:flex-1 lg:text-5xl">
+            {t("savedProfiles")}
+          </div>
+        )}
+      </div>
+      <div className="flex flex-wrap justify-center gap-8">
+        {savedList?.map((profile) => (
+          <div key={profile.id} className="relative cursor-pointer text-center">
+            <img
+              src={profile.avatar_url}
+              alt={profile.login}
+              className="b-white mx-auto h-96 w-96 rounded-xl border-2 border-slate-100 object-cover p-2 lg:w-64"
+            />
             <div className="absolute inset-4 flex items-end justify-between">
-              <MdDeleteForever className="z-10 size-8 text-red-600 hover:text-red-500 duration-300 ease-in-out" onClick={() => {removeProfile(profile.id), !profiles.length && navigate("/profiles")}}/>
+              <MdDeleteForever
+                className="z-10 size-8 text-red-600 duration-300 ease-in-out hover:text-red-500"
+                onClick={() => {
+                  (removeProfile(profile.id),
+                    !savedList.length && navigate("/profiles"));
+                }}
+              />
               <Link to={`/profile/${profile.login}`}>
-                <button className="px-4 py-2 bg-white hover:bg-opacity-50 transition-colors duration-300 ease-in-out rounded-full">{profile.login}</button>
+                <button className="rounded-full bg-white px-4 py-2 transition-colors duration-300 ease-in-out hover:bg-opacity-50">
+                  {profile.login}
+                </button>
               </Link>
             </div>
-        </div>
-      ))}
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default SavedList
+export default SavedList;
