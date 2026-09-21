f7335d7cd4432d53879f15acbf567e963e48da75
therealdanmalmx <danmalmx@gmail.com>
2026-09-21 10:54:30 +0200

    Add additional custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -8,15 +8,19 @@ export default {
       colors: {
         "logo-text": "#D7DBDD",
         navbar: "#212F3D",
+        nordic: "#0b1326",
+        nordic_shade: "#121b2e",
+        nordic_asccent: "#0e1c34",
+        nordic_salmon: "#ffb4ab",
       },
       textColor: {
         "!text-green-500": "#10B981 !important",
         "!text-white": "#FFFFFF !important",
       },
       screens: {
-        "small-screen": {"max": "376px"},
+        "small-screen": { max: "376px" },
         ...defaultTheme.screens,
-      }
+      },
     },
   },
   plugins: [],
