4a11b145dd19ad5a0d44024dcedc5cfbae24d0cd
therealdanmalmx <danmalmx@gmail.com>
2026-09-21 10:56:05 +0200

    Refactor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,21 @@
-import DropdownCountries from './DropdownCountries'
+import DropdownCountries from "./DropdownCountries";
 
 const Footer = () => {
   return (
-    <div className='h-40 lg:h-28 flex flex-col-reverse lg:flex-row lg:justify-between gap-8 lg:gap-0 px-12 bg-navbar fixed w-full bottom-0 items-center justify-center'>
+    <div className="bg-nordic_shade fixed bottom-0 flex h-40 w-full flex-col-reverse items-center justify-center gap-8 px-12 lg:h-28 lg:flex-row lg:justify-between lg:gap-0">
       <div>
-        <a href="https://www.buymeacoffee.com/danmalmx" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style={{height: "40px", width: "152px"}} /></a>
+        <a href="https://www.buymeacoffee.com/danmalmx" target="_blank">
+          <img
+            src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+            alt="Buy Me A Coffee"
+            style={{ height: "40px", width: "152px" }}
+          />
+        </a>
       </div>
-      <div className='hidden'></div>
+      <div className="hidden"></div>
       <DropdownCountries />
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
