db1a0a666642ce583134d8a271dc89066e5ec43a
therealdanmalmx <danmalmx@gmail.com>
2026-09-21 14:50:41 +0200

    Refactor

--- src/views/Profiles.tsx
+++ src/views/Profiles.tsx
@@ -1,62 +1,74 @@
 import { useContext } from "react";
 import { useTranslation } from "react-i18next";
 import { FaChevronCircleLeft } from "react-icons/fa";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 import { CountryContext } from "../Context/CountryContext";
 import { FetchContext } from "../Context/FetchContext";
 import { SearchContext } from "../Context/SearchContext";
 
-
 const Profiles = () => {
-  const { profiles } = useContext(FetchContext)
-  const {selectedIcons, searchText} = useContext(SearchContext)
-  const {formattedCountry} = useContext(CountryContext)
+  const { profiles } = useContext(FetchContext);
+  const { selectedIcons, searchText } = useContext(SearchContext);
+  const { formattedCountry } = useContext(CountryContext);
   const { t } = useTranslation();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const country_code = localStorage.getItem("country_code") ?? "";
 
   const counrySentence = (country_code: string, icons: string) => {
-
-    let sentence:string = "";
-    switch(country_code)
-    {
-        case "es":
-        case "pt":
-        case "fr":
-        case "it":
-          sentence = `${t("developers")} ${icons}`
+    let sentence: string = "";
+    switch (country_code) {
+      case "es":
+      case "pt":
+      case "fr":
+      case "it":
+        sentence = `${t("developers")} ${icons}`;
         break;
-        default:
-          sentence = `${icons} ${t("developers")}`
-        break
-      }
-      return sentence;
-  }
+      default:
+        sentence = `${icons} ${t("developers")}`;
+        break;
+    }
+    return sentence;
+  };
 
   return (
     <div>
-      <div className="flex flex-col lg:flex-row justify-center items-center my-4">
-        <div onClick={() => navigate("/")}><FaChevronCircleLeft className="size-12 mx-auto my-2 lg:mx-16 cursor-pointer hover:bg-slate-500 hover:rounded-full"/></div>
-          {profiles.items.length > 0 && <div className="lg:flex-1 text-center text-2xl lg:text-5xl font-bold uppercase">{counrySentence(country_code, selectedIcons.map((icon => icon === "csharp" ? "C#" : icon)).join(", "))} | {searchText ? searchText : formattedCountry} </div>}
+      <div className="my-4 flex flex-col items-center justify-center lg:flex-row">
+        <div onClick={() => navigate("/")}>
+          <FaChevronCircleLeft className="bg-nord bg-nordic_salmon mx-auto my-2 size-12 cursor-pointer rounded-full hover:rounded-full hover:bg-slate-500 lg:mx-16" />
+        </div>
+        {profiles.items.length > 0 && (
+          <div className="text-nordic_salmon text-center text-2xl font-bold uppercase lg:flex-1 lg:text-5xl">
+            {counrySentence(
+              country_code,
+              selectedIcons
+                .map((icon) => (icon === "csharp" ? "C#" : icon))
+                .join(", "),
+            )}{" "}
+            | {searchText ? searchText : formattedCountry}{" "}
+          </div>
+        )}
       </div>
-      <div className="flex justify-center flex-wrap gap-8">
+      <div className="bg-nordic flex flex-wrap justify-center gap-8">
         {profiles?.items?.map((profile) => (
-          <div
-            key={profile.id}
-            className="text-center cursor-pointer relative"
-          >
-            <img src={profile.avatar_url} alt={profile.login} className="h-96 w-96 lg:w-64 b-white p-2 border-slate-100 border-2 rounded-xl object-cover mx-auto"/>
+          <div key={profile.id} className="relative cursor-pointer text-center">
+            <img
+              src={profile.avatar_url}
+              alt={profile.login}
+              className="b-white mx-auto h-96 w-96 rounded-xl border-2 border-slate-100 object-cover p-2 lg:w-64"
+            />
             <Link to={`/profile/${profile.login}`}>
               <div className="absolute inset-4 flex items-end justify-end">
-                <button className="px-4 py-2 bg-white hover:bg-opacity-50 transition-colors duration-300 ease-in-out rounded-full">{profile.login}</button>
+                <button className="rounded-full bg-white px-4 py-2 transition-colors duration-300 ease-in-out hover:bg-opacity-50">
+                  {profile.login}
+                </button>
               </div>
             </Link>
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profiles
+export default Profiles;
