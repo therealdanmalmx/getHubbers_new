812b1d037c46854fb133cc9206835ed4482ce083
therealdanmalmx <danmalmx@gmail.com>
2026-09-21 14:49:59 +0200

    Show logo text. Adjust styling

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { useTranslation } from "react-i18next";
+import { LuCrosshair } from "react-icons/lu";
 import { CountryContext } from "../Context/CountryContext";
 import { SearchContext } from "../Context/SearchContext";
 
@@ -10,12 +11,11 @@ const SearchField = () => {
 
   return (
     <div
-      className={`mx-auto flex flex-col items-center justify-start space-y-4 pt-20 lg:pt-32 small-screen:justify-start small-screen:pt-8 ${window.innerHeight === 600 && "pt-12"}}`}
+      className={`h-fullz-_300px)] mx-auto flex flex-col items-center justify-center xl:h-[calc(100%_-_200px)] small-screen:justify-start small-screen:pt-8 ${window.innerHeight === 600 && "pt-12"}}`}
     >
-      <h1 className="text-nordic_salmon w-full text-balance text-center text-lg xl:w-[500px]">
-        {t("searchFieldHeading", {
-          formattedCountry,
-        })}
+      <h1 className="text-nordic_salmon bg-nordic_shade my-4 flex w-max items-center gap-2 text-balance rounded-full px-4 py-2 text-center text-lg">
+        <LuCrosshair />
+        {t("logoText")}
       </h1>
       <div className="flex w-11/12 items-center justify-center lg:w-full">
         <input
@@ -23,17 +23,24 @@ const SearchField = () => {
           placeholder={t("searchFieldPlaceholder", {
             formattedCountry,
           })}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              getSearchCity;
+            }
+          }}
           className="bg-nordic_shade text-nordic_salmon border-nordic_shade w-5/6 border-2 border-r-0 p-4 outline-none md:w-96"
         />
         <button
           onClick={getSearchCity}
-          className="border-nordic_salmon bg-nordic_salmon text-nordic border-2 border-l-0 p-4"
+          className="border-nordic_salmon bg-nordic_salmon text-nordic border-2 border-l-0 p-4 font-bold"
         >
           {" "}
-          {/* Search */}
           {t("searchButton", {
             formattedCountry,
           })}
+          <span className="mx-2 rounded bg-red-300 px-3 py-1 font-mono text-base font-semibold">
+            ↵ Enter
+          </span>
         </button>
       </div>
     </div>
